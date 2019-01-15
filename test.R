516194c2cece34ffa1abe7be5d98cb9ca9267ba8
michaelblais <michael-blais@idexx.com>
2019-01-15 17:02:02 -0600

    test

--- test.R
+++ test.R
@@ -1,5 +1,10 @@
+#' ---
+#' output: github_document
+#' ---
+
 require(dplyr)
 
 df1 <- as.data.frame(installed.packages(),row.names = FALSE)
 
 df2 <- df1 %>% select(Package,LibPath,Version,Priority,Built)
+
